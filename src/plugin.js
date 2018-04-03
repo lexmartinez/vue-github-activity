b4839fb4591465e9a8138f9be07508b8d7460d3e
Lex Martinez <lex0316@gmail.com>
2018-04-03 10:32:14 -0500

    event-feed component implementation

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,9 @@
 import GithubFeed from './GithubFeed.vue';
+import FeedEvent from './FeedEvent.vue';
 
 module.exports = {
   install: function (Vue, options) {
     Vue.component('github-feed', GithubFeed);
+    Vue.component('feed-event', FeedEvent);
   }
 };
