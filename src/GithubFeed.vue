b57bcde9dac4af2b2f1b740b5b6fb36a13d96d5d
Lex Martinez <lex0316@gmail.com>
2018-04-03 08:46:01 -0500

    component template - styling feed header

--- src/GithubFeed.vue
+++ src/GithubFeed.vue
@@ -1,8 +1,23 @@
 <template>
-    <div>
-        <h1>{{user.name}} <small>{{user.login}}</small></h1>
-        <img :src="user.avatar_url"/>
-        <h3>{{events.length}}</h3>
+    <div class="feed">
+        <div class="header-wrapper">
+            <div class="header">
+                <div class="github-icon">
+                    <span class="github-octicon octicon octicon-mark-github"></span>
+                </div>
+                <a :href="user.html_url" target="_blank" class="github-profile">
+                    <div style="font-size: 26px; line-height: 30px">
+                        {{user.name}}
+                    </div>
+                    <div class="github-username">
+                        {{user.login}}
+                    </div>
+                </a>
+                <div class="avatar-wrapper">
+                    <img class="avatar" :src="user.avatar_url" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -43,5 +58,69 @@
 </script>
 
 <style scoped>
+    .feed {
+        position: relative;
+        display: table;
+        border: 1px solid #ddd;
+        height: 100%;
+        min-height: 200px;
+        width: 100%;
+        min-width: 500px;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif;
+        font-size: 1rem;
+        color: #373a3c
+    }
+    
+    .header-wrapper {
+        display: table-row;
+        height: 1px
+    }
+    .header {
+        display: table-cell;
+        top: 1px;
+        left: 1px;
+        padding: 10px;
+        border-bottom: 1px solid #ddd
+    }
 
+    .avatar-wrapper {
+        display: inline;
+        float: right;
+        margin-right: 10px;
+        padding-right: 20px;
+        max-width: 60px;
+        height: 67px;
+    }
+    .avatar {
+        padding: 3px;
+        height: 100%;
+        border: 1px solid #ddd;
+        box-shadow: 1px 1px 3px #ccc;
+    }
+    .github-octicon {
+        font: normal normal 40px 'octicons'
+    }
+    .github-icon {
+        display: inline;
+        float: left;
+        padding: 9px 0 0;
+        width: 35px;
+        height: 35px;
+    }
+    .github-username {
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: 24px;
+        color: #666;
+    }
+    .github-profile {
+        display: inline-block;
+        float: left;
+        margin: 0 auto;
+        padding: 6px 10px 5px;
+        color: #495961;
+        font-size: 20px;
+        text-decoration: none
+    }
 </style>
